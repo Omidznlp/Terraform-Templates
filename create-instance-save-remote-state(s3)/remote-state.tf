cfda431f65b008137ff2b2cb4964347ba0bd8f41
omid <o.zeynalpour@gmail.com>
2022-05-10 17:18:44 +0200

    change reomte-state

--- create-instance-save-remote-state(s3)/remote-state.tf
+++ create-instance-save-remote-state(s3)/remote-state.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "bucket-for-save-filestate-terraform"
-        key    = "terraform_state"
+        key    = "terraform_state.tf"
         region = "eu-north-1"
     }
 }
